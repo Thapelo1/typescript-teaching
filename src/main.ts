af8809783fa49a86ca666634f9e3299b19061a2b
Thapelo <Thapelo.Kekana1@fnb.co.za>
2022-08-01 01:10:06 +0200

    utility-types

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+/**
+ * TypeScript Utility Types:
+ * TypeScript comes with a large number of types that can help with 
+    some common type manipulation, usually referred to as utility types.
+ */
